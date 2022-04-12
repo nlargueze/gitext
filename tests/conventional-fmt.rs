c59010b3545d67aaa399a3e14caf63375990dccd
Nicolas Largueze <nick@nicklabs.io>
2022-04-12 13:08:38 +0200

    refactor: Refactored code

---
+++ tests/conventional-fmt.rs
@@ -0,0 +1,126 @@
+//! Testing of conventional commits
+
+use gitt::conventional::ConventionalCommit;
+use indoc::indoc;
+
+#[test]
+fn oneliner_no_scope() {
+    let commit = ConventionalCommit {
+        r#type: "fix".to_string(),
+        scope: None,
+        subject: "commit subject".to_string(),
+        body: None,
+        breaking_change: None,
+        closed_issues: None,
+    };
+
+    let msg = indoc!("fix: commit subject");
+
+    assert_eq!(commit.to_string(), msg);
+}
+
+#[test]
+fn oneliner_scope() {
+    let commit = ConventionalCommit {
+        r#type: "fix".to_string(),
+        scope: Some("myscope".to_string()),
+        subject: "commit subject".to_string(),
+        body: None,
+        breaking_change: None,
+        closed_issues: None,
+    };
+
+    let msg = indoc!("fix(myscope): commit subject");
+
+    assert_eq!(commit.to_string(), msg);
+}
+
+#[test]
+fn oneliner_scope_breaking_change() {
+    let commit = ConventionalCommit {
+        r#type: "fix".to_string(),
+        scope: Some("myscope".to_string()),
+        subject: "commit subject".to_string(),
+        body: None,
+        breaking_change: Some("breaking change".to_string()),
+        closed_issues: None,
+    };
+
+    let msg = indoc!(
+        "fix(myscope)!: commit subject
+    
+    BREAKING CHANGE: breaking change"
+    );
+
+    assert_eq!(commit.to_string(), msg);
+}
+
+#[test]
+fn body() {
+    let commit = ConventionalCommit {
+        r#type: "fix".to_string(),
+        scope: Some("myscope".to_string()),
+        subject: "commit subject".to_string(),
+        body: Some("Commit body\nAnother body line".to_string()),
+        breaking_change: None,
+        closed_issues: None,
+    };
+
+    let msg = indoc!(
+        "fix(myscope): commit subject
+    
+    Commit body
+    Another body line"
+    );
+
+    assert_eq!(commit.to_string(), msg);
+}
+
+#[test]
+fn body_with_issues() {
+    let commit = ConventionalCommit {
+        r#type: "fix".to_string(),
+        scope: Some("myscope".to_string()),
+        subject: "commit subject".to_string(),
+        body: Some("Commit body\nAnother body line".to_string()),
+        breaking_change: None,
+        closed_issues: Some(vec![1, 2]),
+    };
+
+    let msg = indoc!(
+        "fix(myscope): commit subject
+    
+    Commit body
+    Another body line
+    
+    Closes #1
+    Closes #2"
+    );
+
+    assert_eq!(commit.to_string(), msg);
+}
+
+#[test]
+fn body_with_change_and_issues() {
+    let commit = ConventionalCommit {
+        r#type: "fix".to_string(),
+        scope: Some("myscope".to_string()),
+        subject: "commit subject".to_string(),
+        body: Some("Commit body\nAnother body line".to_string()),
+        breaking_change: Some("A breaking change".to_string()),
+        closed_issues: Some(vec![1, 2]),
+    };
+
+    let msg = indoc!(
+        "fix(myscope)!: commit subject
+    
+    Commit body
+    Another body line
+    
+    BREAKING CHANGE: A breaking change
+    Closes #1
+    Closes #2"
+    );
+
+    assert_eq!(commit.to_string(), msg);
+}
---
+++ tests/conventional-parse.rs
@@ -0,0 +1,235 @@
+//! Testing of conventional commits parsing
+
+use gitt::{config::Config, conventional::ConventionalCommit};
+use indoc::indoc;
+
+#[test]
+fn oneliner_simple() {
+    let msg = indoc!("fix: commit subject");
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: None,
+            breaking_change: None,
+            closed_issues: None
+        }
+    )
+}
+
+#[test]
+fn oneliner_scope() {
+    let msg = indoc!("fix(myscope): commit subject");
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: Some("myscope".to_string()),
+            subject: "commit subject".to_string(),
+            body: None,
+            breaking_change: None,
+            closed_issues: None
+        }
+    )
+}
+
+#[test]
+fn oneliner_breaking() {
+    let msg = indoc!("fix!: commit subject");
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: None,
+            breaking_change: Some("".to_string()),
+            closed_issues: None
+        }
+    )
+}
+
+#[test]
+fn oneliner_scope_breaking() {
+    let msg = indoc!("fix(myscope)!: commit subject");
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: Some("myscope".to_string()),
+            subject: "commit subject".to_string(),
+            body: None,
+            breaking_change: Some("".to_string()),
+            closed_issues: None
+        }
+    )
+}
+
+#[test]
+fn body_simple() {
+    let msg = indoc!(
+        "fix: commit subject
+
+    commit body"
+    );
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: Some("commit body".to_string()),
+            breaking_change: None,
+            closed_issues: None
+        }
+    )
+}
+
+#[test]
+fn body_multiline_simple() {
+    let msg = indoc!(
+        "fix: commit subject
+
+    commit body
+    commit body line 2"
+    );
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: Some("commit body\ncommit body line 2".to_string()),
+            breaking_change: None,
+            closed_issues: None
+        }
+    )
+}
+
+#[test]
+fn body_multiline_breaking_change_no_issues() {
+    let msg = indoc!(
+        "fix: commit subject
+
+    commit body
+    commit body line 2
+    
+    BREAKING CHANGE: this is a breaking change
+    on several line"
+    );
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: Some("commit body\ncommit body line 2".to_string()),
+            breaking_change: Some("this is a breaking change\non several line".to_string()),
+            closed_issues: None
+        }
+    )
+}
+
+#[test]
+fn body_multiline_breaking_change_issues() {
+    let msg = indoc!(
+        "fix: commit subject
+
+    commit body
+    commit body line 2
+    
+    BREAKING CHANGE: this is a breaking change
+    on several line
+    Closes #1"
+    );
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: Some("commit body\ncommit body line 2".to_string()),
+            breaking_change: Some("this is a breaking change\non several line".to_string()),
+            closed_issues: Some(vec![1])
+        }
+    )
+}
+
+#[test]
+fn body_multiline_issues() {
+    let msg = indoc!(
+        "fix: commit subject
+
+    commit body
+    commit body line 2
+    
+    Closes #1"
+    );
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: Some("commit body\ncommit body line 2".to_string()),
+            breaking_change: None,
+            closed_issues: Some(vec![1])
+        }
+    )
+}
+
+#[test]
+fn body_multiline_issues_2() {
+    let msg = indoc!(
+        "fix: commit subject
+
+    commit body
+    commit body line 2
+    
+    Closes #1
+    Closes #2"
+    );
+
+    let commit = ConventionalCommit::parse(&msg, &Config::default()).unwrap();
+
+    assert_eq!(
+        commit,
+        ConventionalCommit {
+            r#type: "fix".to_string(),
+            scope: None,
+            subject: "commit subject".to_string(),
+            body: Some("commit body\ncommit body line 2".to_string()),
+            breaking_change: None,
+            closed_issues: Some(vec![1, 2])
+        }
+    )
+}
