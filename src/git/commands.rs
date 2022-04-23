d5d56c7b4d214741a421499a0e9ecae50a481c47
Nicolas Largueze <nick@nicklabs.io>
2022-04-23 20:51:26 +0200

    refactor(git_log): moved git_log() to another file

--- src/git/commands.rs
+++ src/git/commands.rs
@@ -3,7 +3,6 @@
 pub mod add;
 pub mod commit;
 pub mod config;
-pub mod logg;
 pub mod push;
 pub mod status;
 pub mod tag;
@@ -11,7 +10,6 @@ pub mod tag;
 pub use add::*;
 pub use commit::*;
 pub use config::*;
-pub use logg::*;
 pub use push::*;
 pub use status::*;
 pub use tag::*;
